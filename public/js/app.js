d420e84b4f5e92d452ed1a7894d43f6abad9b922
giovannymevi <giovannymendozavillamizar1@gmail.com>
2026-03-09 09:30:24 -0400

    tercer

--- public/js/app.js
+++ public/js/app.js
@@ -101,3 +101,86 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
+
+/* Configuración de partículas para la sección de proyectos */
+particlesJS("particles-proyectos", {
+  "particles": {
+    "number": {
+      "value": 50, /* Menos partículas para un look más limpio */
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#39ff14" /* Color luz fosforecente */
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
+    },
+    "opacity": {
+      "value": 0.6, /* Un poco más visibles */
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#39ff14", /* Color luz fosforecente */
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 4, /* Un poco más lento */
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble" /* Efecto burbuja */
+      },
+      "onclick": {
+        "enable": false /* Desactivado para no interferir con los links */
+      },
+      "resize": true
+    },
+    "modes": {
+      "bubble": {
+        "distance": 250,
+        "size": 8,
+        "duration": 2,
+        "opacity": 0.8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      }
+    }
+  },
+  "retina_detect": true
+});
